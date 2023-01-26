46feb006866369a1b0ca9fc486652a20ad29a06c
doishub <daniele@oveleon.de>
2023-01-26 16:52:01 +0100

    Update scripts

--- src/Resources/public/src/index.js
+++ src/Resources/public/src/index.js
@@ -1,25 +1,40 @@
 import 'animate.css';
 
-window.initAnimation = (inViewOffset = '50px') => {
-    const animateStartClassName = 'animate__animated'
-    const animatePrefix = 'animate__'
+window.initAnimation = (opts) => {
+    const defaultOptions = {
+        inView: {
+            root: null,
+            offset: '50px',
+            threshold: 0.5
+        },
+        animate: {
+            children: null,
+            selectors: {
+                prefix:   'animate__',
+                perform:  'animate__animated',
+                trigger:  'anim-tg-',
+                in:       'anim-in-',
+                out:      'anim-out-',
+                delay:    'anim-dly-',
+                duration: 'anim-dtn-'
+            }
+        }
+    }
 
-    const animationTriggerSelector = 'anim-tg-'
-    const animationInSelector = 'anim-in-'
-    const animationOutSelector = 'anim-out-'
+    const options = {...defaultOptions, ...opts}
 
     const animate = (el, animationName, removeFromClasses) => {
         if(removeFromClasses)
-            el.classList.remove(animateStartClassName, animatePrefix + removeFromClasses)
+            el.classList.remove(options.animate.selectors.perform, options.animate.selectors.prefix + removeFromClasses)
 
-        el.classList.add(animateStartClassName, animatePrefix + animationName)
+        el.classList.add(options.animate.selectors.perform, options.animate.selectors.prefix + animationName)
     }
 
     const animateInView = (observerElement, enter, exit) => {
-        const options = {
-            root: null,
-            rootMargin: inViewOffset,
-            threshold: 0.5
+        const observerOptions = {
+            root:  options.inView.root,
+            rootMargin: options.inView.offset,
+            threshold: options.inView.threshold
         };
 
         const callback = (entries) => {
@@ -30,7 +45,7 @@ window.initAnimation = (inViewOffset = '50px') => {
             });
         };
 
-        (new IntersectionObserver(callback, options)).observe(observerElement);
+        (new IntersectionObserver(callback, observerOptions)).observe(observerElement);
     }
 
     const getClassNameByPrefix = (el, prefix) => {
@@ -40,16 +55,34 @@ window.initAnimation = (inViewOffset = '50px') => {
         return el.className.split(prefix)[1].split(" ")[0]
     }
 
-    const animationElements = document.querySelectorAll('[class*="' + animationTriggerSelector + '"]')
+    const initAnimationItem = (el, animationElement, index) => {
+        // Get trigger information
+        let animationTrigger = getClassNameByPrefix(el, options.animate.selectors.trigger)
 
-    animationElements.forEach((el) => {
-        // Get animation trigger
-        const animationTrigger = getClassNameByPrefix(el, animationTriggerSelector)
-        const animationElement = el.querySelector(':scope > .inside') ? el.querySelector(':scope > .inside') : el
+        // Get delay and duration information
+        let animationDelay = getClassNameByPrefix(el, options.animate.selectors.delay)
+        let animationDuration = getClassNameByPrefix(el, options.animate.selectors.duration)
+
+        // Duration works fine with custom props
+        if(animationDuration)
+            animationElement.style.setProperty('--animate-duration', animationDuration.replace('-', '.') + 's')
+
+        // Instead of overriding the custom-property as we did with the duration, we have to take a different way
+        // for now because of a bug. (https://github.com/animate-css/animate.css/issues/1544)
+        if(animationDelay){
+            animationDelay = parseFloat(animationDelay.replace('-', '.'))
+
+            if(options.animate.children) {
+                animationDelay = animationDelay * index
+            }
+
+            animationElement.style.setProperty('--animate-delay', animationDelay + 's')
+            animationElement.classList.add('animate__delay-1s')
+        }
 
         // Get animations
-        const inAnimationName = getClassNameByPrefix(el, animationInSelector)
-        const outAnimationName = getClassNameByPrefix(el, animationOutSelector)
+        const inAnimationName = getClassNameByPrefix(el, options.animate.selectors.in)
+        const outAnimationName = getClassNameByPrefix(el, options.animate.selectors.out)
 
         switch (animationTrigger)
         {
@@ -67,6 +100,16 @@ window.initAnimation = (inViewOffset = '50px') => {
 
             default:
                 animate(animationElement, inAnimationName, outAnimationName)
+        }
+    }
+
+    document.querySelectorAll('[class*="' + options.animate.selectors.trigger + '"]')?.forEach((el) => {
+        if(options.animate.children){
+            el.querySelectorAll(options.animate.children + ' > *').forEach((animationElement, index) => {
+                initAnimationItem(el, animationElement, index)
+            })
+        }else{
+            initAnimationItem(el, el.querySelector(':scope > .inside') ? el.querySelector(':scope > .inside') : el, 0)
         }
     })
 }
